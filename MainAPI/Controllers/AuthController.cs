67a13949fefadefe748ecc81d22e08796761802e
henimex <henimex@gmail.com>
2023-01-07 22:22:56 +0300

    Authentication

--- MainAPI/Controllers/AuthController.cs
+++ MainAPI/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MainAPI.Data;
+using MainAPI.Dtos;
+using MainAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MainAPI.Controllers
@@ -7,5 +10,30 @@ namespace MainAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            //Validation Operations
+
+            userForRegisterDto.Username = userForRegisterDto.Username.Trim().ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("User Alrready Exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username,
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+            return StatusCode(201);
+        }
     }
 }
