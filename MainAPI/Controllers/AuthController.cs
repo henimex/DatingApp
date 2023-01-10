81869fb5b2fb356ce8ddba34eb459f234f06f792
henimex <henimex@gmail.com>
2023-01-11 00:13:03 +0300

    Authentication

--- MainAPI/Controllers/AuthController.cs
+++ MainAPI/Controllers/AuthController.cs
@@ -1,8 +1,12 @@
-﻿using MainAPI.Data;
+﻿using System.Security.Claims;
+using System.Text;
+using MainAPI.Data;
 using MainAPI.Dtos;
 using MainAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace MainAPI.Controllers
 {
@@ -11,10 +15,12 @@ namespace MainAPI.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -34,6 +40,35 @@ namespace MainAPI.Controllers
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForRegisterDto userForRegisterDto)
+        {
+            var userFromRepo = await _repo.Login(userForRegisterDto.Username, userForRegisterDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value!));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler(tokenDescriptor);
         }
     }
 }
--- MainAPI/Dtos/UserForRegisterDto.cs
+++ MainAPI/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
-﻿namespace MainAPI.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MainAPI.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Min 4 Max 8 Chars")]
         public string Password { get; set; }
     }
 }
